429ce1228a9d219d9ad6991c3d2b172f738344a5
Saurabh Pandey <1991randomguy@gmail.com>
2026-08-08 09:46:09 +0000

    k

--- models/daily_weather.sql
+++ models/daily_weather.sql
@@ -4,20 +4,21 @@ with
         from {{ source("db_src", "weather") }}
     ),
 
-cte_weather_agg as 
-(
-select 
-daily_weather,
-weather,
-avg(clouds) as clouds,
-avg(humidity) as humidity,
-avg(pressure) as pressure,
-avg(temp) as temp
-qualify row_number() over(partiton by daily_weather order by count(weather) desc)=1
-from cte_weather 
-group by daily_weather
+    cte_weather_agg as (
+        select
+            daily_weather,
+            weather,
+            avg(clouds) as clouds,
+            avg(humidity) as humidity,
+            avg(pressure) as pressure,
+            avg(temp) as temp
+        from cte_weather
+        group by daily_weather, weather
+        qualify
+            row_number() over (partition by daily_weather order by count(weather) desc)
+            = 1
 
-)
+    )
 
 select *
 from cte_weather_agg
