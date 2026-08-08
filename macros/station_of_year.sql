56eac2df6b77efd01f1738767b3f761524e333e1
Saurabh Pandey <1991randomguy@gmail.com>
2026-08-08 09:08:36 +0000

    k

--- macros/station_of_year.sql
+++ macros/station_of_year.sql
@@ -1,10 +1,10 @@
-{% macro station_of_year() %}
+{% macro station_of_year(x) %}
     case
-        when month(to_timestamp(started_at)) in (12, 1, 2)
+        when month(to_timestamp(x)) in (12, 1, 2)
         then 'WINTER'
-        when month(to_timestamp(started_at)) in (3, 4, 5)
+        when month(to_timestamp(x)) in (3, 4, 5)
         then 'SPRING'
-        when month(to_timestamp(started_at)) in (6, 7, 8)
+        when month(to_timestamp(x)) in (6, 7, 8)
         then 'SUMMER'
         else 'AUTUMN'
     end 
--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -9,16 +9,7 @@ with
                 then 'WEEKND'
                 else 'BUSINESSDAY'
             end as day_type,
-            case
-                when month(to_timestamp(started_at)) in (12, 1, 2)
-                then 'WINTER'
-                when month(to_timestamp(started_at)) in (3, 4, 5)
-                then 'SPRING'
-                when month(to_timestamp(started_at)) in (6, 7, 8)
-                then 'SUMMER'
-                else 'AUTUMN'
-            end as station_of_year,
-            {{ station_of_year() }} as station_of_year_macro
+            {{ station_of_year("started_at") }} as station_of_year
 
         from {{ source("db_src", "bike") }}
         where started_at != 'started_at'
