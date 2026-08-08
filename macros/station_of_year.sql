2059b2a5a22d9fb175410a926b6a8ac76599fd8e
Saurabh Pandey <1991randomguy@gmail.com>
2026-08-08 09:13:47 +0000

    l

--- macros/station_of_year.sql
+++ macros/station_of_year.sql
@@ -1,10 +1,10 @@
-{% macro station_of_year(x) %}
+{% macro station_of_year(column_name) %}
     case
-        when month(to_timestamp(x)) in (12, 1, 2)
+        when month(to_timestamp(column_name)) in (12, 1, 2)
         then 'WINTER'
-        when month(to_timestamp(x)) in (3, 4, 5)
+        when month(to_timestamp(column_name)) in (3, 4, 5)
         then 'SPRING'
-        when month(to_timestamp(x)) in (6, 7, 8)
+        when month(to_timestamp(column_name)) in (6, 7, 8)
         then 'SUMMER'
         else 'AUTUMN'
     end 
